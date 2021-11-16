9510f028d2de577bc34f2a52c16c1e046c6a67d2
alaneicho <93944293+alaneicho@users.noreply.github.com>
2021-11-16 17:41:09 -0300

    Add Lista::eliminar

--- src/Lista.hpp
+++ src/Lista.hpp
@@ -3,7 +3,7 @@
 Lista::Lista() : longitud_(0), primero_(nullptr), ultimo_(nullptr) {
 }
 
-Lista::Lista(const Lista& l) : Lista() {
+Lista::Lista(const Lista &l) : Lista() {
     //Inicializa una lista vacía y luego utiliza operator= para no duplicar el código de la copia de una lista.
     *this = l;
 }
@@ -12,28 +12,28 @@ Lista::~Lista() {
     // Completar
 }
 
-Lista& Lista::operator=(const Lista& aCopiar) {
+Lista &Lista::operator=(const Lista &aCopiar) {
     // Completar
     return *this;
 }
 
-void Lista::agregarAdelante(const int& elem) {
-    Nodo* nuevo_primero = new Nodo(elem);
+void Lista::agregarAdelante(const int &elem) {
+    Nodo *nuevo_primero = new Nodo(elem);
     nuevo_primero->proximo = this->primero_;
     //struct Nodo nuevo_primero = {nullptr, elem, this->primero_ };
     this->primero_ = nuevo_primero;
-    if (longitud_ == 0){
+    if (longitud_ == 0) {
         this->ultimo_ = nuevo_primero;
     }
     this->longitud_++;
 }
 
-void Lista::agregarAtras(const int& elem) {
-    Nodo* nuevo_ultimo = new Nodo(elem);
+void Lista::agregarAtras(const int &elem) {
+    Nodo *nuevo_ultimo = new Nodo(elem);
     nuevo_ultimo->anterior = this->ultimo_;
     //struct Nodo nuevo_ultimo = {this->ultimo_, elem, nullptr};
     this->ultimo_ = nuevo_ultimo;
-    if (longitud_ == 0){
+    if (longitud_ == 0) {
         this->primero_ = nuevo_ultimo;
     }
     this->longitud_++;
@@ -41,15 +41,24 @@ void Lista::agregarAtras(const int& elem) {
 
 void Lista::eliminar(Nat i) {
     // Completar
+    Nodo *nodo_byebye = this->primero_;
+    while (i > 0) {
+        nodo_byebye = nodo_byebye->proximo;
+        i--;
+    }
+    nodo_byebye->anterior->proximo = nodo_byebye->proximo;
+    nodo_byebye->proximo->anterior = nodo_byebye->anterior;
+
+    delete nodo_byebye;
 }
 
 int Lista::longitud() const {
     return longitud_;
 }
 
-const int& Lista::iesimo(Nat i) const {
-    Nodo* nodo_actual = this->primero_;
-    while (i > 0){
+const int &Lista::iesimo(Nat i) const {
+    Nodo *nodo_actual = this->primero_;
+    while (i > 0) {
         nodo_actual = nodo_actual->proximo;
         i--;
     }
@@ -58,10 +67,10 @@ const int& Lista::iesimo(Nat i) const {
     //assert(false);
 }
 
-int& Lista::iesimo(Nat i) {
+int &Lista::iesimo(Nat i) {
 
-    Nodo* nodo_actual = this->primero_;
-    while (i > 0){
+    Nodo *nodo_actual = this->primero_;
+    while (i > 0) {
         nodo_actual = nodo_actual->proximo;
         i--;
     }
@@ -70,6 +79,6 @@ int& Lista::iesimo(Nat i) {
     //assert(false);
 }
 
-void Lista::mostrar(ostream& o) {
+void Lista::mostrar(ostream &o) {
     // Completar
 }
